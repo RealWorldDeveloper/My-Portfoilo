35d81abe9c3f82e84581e8c18600c189b18839cd
Md Ain Uddin Ahmmad <real.world.developer@gmail.com>
2024-08-16 19:50:17 +0100

    forth

--- Assets/skill.js
+++ Assets/skill.js
@@ -2,26 +2,26 @@ export const mySkill = [
 {
  Title: 'Web Development',
  Description: ' Highly skilled on creating web application by HTML,css,javascript, react,bootstrap anf much more',
- Image: '/images/Front.png'
+ Image: '/public/images/Front.png'
 },
 {
   Title: 'App Development',
   Description: 'Can Collaborate with othes for App Development',
-  Image: '/images/app.png' 
+  Image: '/public/images/app.png' 
  },
  {
   Title: 'UI/UX Development',
   Description: 'Design Specialist on Front End Development with javascript Library such as react',
-  Image: '/images/ui.png' 
+  Image: '/public/images/ui.png' 
  },
  {
   Title: 'BeckEnd Development',
   Description: 'Expert on Beckend Development with node.js, express.js and knowledge on MERN',
-  Image: '/images/beck.png' 
+  Image: '/public/images/beck.png' 
  },
   {
     Title: 'Debugging',
     Description: 'Able to find bug and debugging with using devTools',
-    Image: '/images/debug.png' 
+    Image: '/public/images/debug.png' 
    },
 ]
