9a6f7a0aeca3c13bdc16fcefd97187c290ee92d9
Md Ain Uddin Ahmmad <real.world.developer@gmail.com>
2024-10-18 13:08:10 +0100

    Updated on 18 oct 2024

--- Components/Qualifications/Qualification.jsx
+++ Components/Qualifications/Qualification.jsx
@@ -16,7 +16,7 @@ function Qualification() {
                 <div class="card h-100 py-2" id="card-qualification">
                 <img src={items.image} class="card-img-top" alt="..." />
                 <div class="card-body">
-                  <h3 >Title: {items.Title}</h3>
+                  <h5 style={{color:'#FF004F'}}>Title: {items.Title}</h5>
                   <h4>Level: {items.Level}</h4>
                   <h5 >Year: {items.Year}</h5>
                   <h6>Institution: {items.Institution}</h6>
