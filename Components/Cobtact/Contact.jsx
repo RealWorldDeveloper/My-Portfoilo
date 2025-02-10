74e57e11681b08689e553a1859d914d06b3a09e4
Md Ain Uddin Ahmmad <real.world.developer@gmail.com>
2025-02-10 19:35:31 +0000

    Update Contact.jsx

--- Components/Cobtact/Contact.jsx
+++ Components/Cobtact/Contact.jsx
@@ -34,10 +34,6 @@ function Contact() {
 
             <div class="card-body">
             <h2 className="contact-title">Lets talk</h2>
-            <p class="card-text text-light">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
               <h5 class="card-title fs-6 py-2 text-light"><i class="fa-solid fa-envelope px-2"></i>real.world.developer@gmail.com</h5>
               <h5 class="card-title fs-6 py-2 text-light"><i class="fa-solid fa-phone px-2"></i>+4407428172421</h5>
               <h5 class="card-title fs-6 py-2 text-light"><i class="fa-solid fa-location-dot px-2"></i>London, United Kingdom</h5>
